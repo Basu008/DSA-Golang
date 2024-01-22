de5a86d20e01ac3c9e65dc02182b863a8e5cf147
Basu Singh <basu.singh@hypd.in>
2024-01-22 17:48:01 +0530

    diameter of tree

--- binarytree/ques.go
+++ binarytree/ques.go
@@ -10,3 +10,39 @@ func DepthOfTree(root *Node) int {
 	rightHeight := DepthOfTree(root.Right)
 	return 1 + int(math.Max(float64(leftHeight), float64(rightHeight)))
 }
+
+func IsTreeBalanced(root *Node) bool {
+	if root == nil {
+		return true
+	}
+	leftHeight := DepthOfTree(root.Left)
+	rightHeight := DepthOfTree(root.Right)
+	var diff int
+	if leftHeight >= rightHeight {
+		diff = leftHeight - rightHeight
+	} else {
+		diff = rightHeight - leftHeight
+	}
+	if diff > 1 {
+		return false
+	}
+	left := IsTreeBalanced(root.Left)
+	right := IsTreeBalanced(root.Right)
+	if !left || !right {
+		return false
+	}
+	return true
+}
+
+func DiameterOfTree(root *Node, max []int) int {
+	if root == nil {
+		return 0
+	}
+	leftMax := DiameterOfTree(root.Left, max)
+	rightMax := DiameterOfTree(root.Right, max)
+	dia := leftMax + rightMax
+	if max[0] <= dia {
+		max[0] = dia
+	}
+	return 1 + int(math.Max(float64(leftMax), float64(rightMax)))
+}
--- main.go
+++ main.go
@@ -6,11 +6,6 @@ import (
 )
 
 func main() {
-	// st := stack.Stack{}
-	// for i := 5; i >= 0; i-- {
-	// 	st.Push(i)
-	// }
-	// st.Display()
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	var root *binarytree.Node
 	for _, e := range arr {
@@ -18,4 +13,7 @@ func main() {
 	}
 	binarytree.LevelOrder(root)
 	fmt.Println("Depth of tree", binarytree.DepthOfTree(root))
+	max := []int{0}
+	binarytree.DiameterOfTree(root, max)
+	fmt.Println("Diameter", max)
 }
